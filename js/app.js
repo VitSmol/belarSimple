9e017565871f8fca70b86ef21985e877c65bafd8
VitSmol <smolitskiy@mail.ru>
2024-02-20 11:43:25 +0300

    корректная работа меню

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
 const menuBtn = document.querySelector('.menu');
 const closeBtn = document.querySelector('.close');
 const openClose = (open, close, menu, addClose) => {
-  open.style.display = 'none';
-  close.style.display = 'inline-block';
-  addClose === "add" ? menu.classList.add('active'): menu.classList.remove('active'); //
+  // open.style.display = 'none';
+  // close.style.display = 'inline-block';
+  // addClose === "add" ? menu.classList.add('active'): menu.classList.remove('active'); //
+  if (addClose == 'add') {
+    open.classList.remove('show');
+    close.classList.add('show');
+    menu.classList.add('active');
+  } else {
+    open.classList.add('show');
+    close.classList.remove('show');
+    menu.classList.remove('active');
+  }
 }
 
 menuBtn.addEventListener('click', () => {
   const listMenu = document.querySelector('#listMenu');
   openClose(menuBtn, closeBtn, listMenu, 'add');
   closeBtn.addEventListener('click', () => {
-    openClose(closeBtn, menuBtn,listMenu, 'remove');
+    openClose(menuBtn,closeBtn,listMenu, 'remove');
   })
 })
